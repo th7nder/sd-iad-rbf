578cafc863e1543907c2fcdb0a1fe79246b46b7e
th7nder <vesphy@gmail.com>
2019-06-01 11:14:14 +0200

    Refactor network

--- src/main/kotlin/network/Network.kt
+++ src/main/kotlin/network/Network.kt
@@ -2,28 +2,30 @@ package network
 
 import math.Point
 
-class Network(
-    dimension: Int,
-    sigmas : List<Double>,
-    centers: List<Point>,
-    numLinearNeurons: Int
-) {
-    val numRadialNeurons = sigmas.size
+class Network(val numRadialNeurons : Int) {
+    private val radialLayer = ArrayList<RadialNeuron>()
+    val outputLayer = ArrayList<LinearNeuron>()
 
-    private val radialLayer = ArrayList<RadialNeuron>(numRadialNeurons)
-    val outputLayer = ArrayList<LinearNeuron>(numLinearNeurons)
+    constructor(dimension: Int, sigmas : List<Double>, centers: List<Point>, numLinearNeurons: Int) : this(sigmas.size) {
+        createRadialLayer(sigmas, centers)
+        createLinearLayer(dimension, numLinearNeurons)
+    }
 
-    init {
+    private fun createRadialLayer(sigmas: List<Double>, centers: List<Point>) {
         for (i in 0 until numRadialNeurons) {
             radialLayer.add(RadialNeuron(centers[i], sigmas[i]))
         }
+    }
 
+    private fun createLinearLayer(dimension: Int, numLinearNeurons: Int) {
         for (i in 1..numLinearNeurons) {
             val linearNeuron = LinearNeuron(dimension, numRadialNeurons)
             linearNeuron.connect(radialLayer)
             outputLayer.add(linearNeuron)
         }
     }
+
+
 
     fun output(x: Point) : List<Double> = outputLayer.map { it.output(x) }
 }
