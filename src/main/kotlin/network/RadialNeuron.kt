532750ef76e7e544a169685cb201b8ede932a3f2
th7nder <vesphy@gmail.com>
2019-06-01 10:50:25 +0200

    Refactor radialFunction name

--- src/main/kotlin/network/RadialNeuron.kt
+++ src/main/kotlin/network/RadialNeuron.kt
@@ -4,9 +4,9 @@ import math.Point
 import kotlin.math.pow
 
 class RadialNeuron(private val center : Point, private val sigma : Double) {
-    fun output(x : Point) = R(center.distance(x))
+    fun output(x : Point) = radialFunction(center.distance(x))
 
-    fun R(d: Double) = Math.exp(
+    fun radialFunction(d: Double) = Math.exp(
         -d.pow(2) / (2 * sigma.pow(2))
     )
 }
