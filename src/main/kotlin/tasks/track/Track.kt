234d698a4c7c0845372442979a5015f179bf13fd
Konrad Stepniak <vesphy@gmail.com>
2020-06-16 16:56:07 +0200

    Add przemek mail

--- src/main/kotlin/tasks/track/Track.kt
+++ src/main/kotlin/tasks/track/Track.kt
@@ -49,34 +49,35 @@ fun createChart(
             .height(800)
             .title(title).build()
 
-    val input = chart.addSeries("Before", inputData.project(0), inputData.project(1))
-    input.lineStyle = SeriesLines.NONE
-    input.marker = SeriesMarkers.CIRCLE
-
-    val test = chart.addSeries("After", outputData.project(0), outputData.project(1))
-    test.lineStyle = SeriesLines.NONE
-    test.marker = SeriesMarkers.CIRCLE
-
     val test2 = chart.addSeries("Dane wzorcowe", referenceData.project(0), referenceData.project(1))
     test2.lineStyle = SeriesLines.NONE
     test2.marker = SeriesMarkers.CIRCLE
+
+    val input = chart.addSeries("Przed nauką", inputData.project(0), inputData.project(1))
+    input.lineStyle = SeriesLines.NONE
+    input.marker = SeriesMarkers.CIRCLE
+
+    val test = chart.addSeries("Po nauce", outputData.project(0), outputData.project(1))
+    test.lineStyle = SeriesLines.NONE
+    test.marker = SeriesMarkers.CIRCLE
+
     return chart
 }
 
 
 fun main() {
     val datas = arrayListOf(
-        DataLoader.loadCSV("car_1.csv", 2, 2),
-        DataLoader.loadCSV("car_2.csv", 2, 2),
-        DataLoader.loadCSV("car_3.csv", 2, 2),
-        DataLoader.loadCSV("car_5.csv", 2, 2),
-        DataLoader.loadCSV("car_6.csv", 2, 2),
-        DataLoader.loadCSV("car_7.csv", 2, 2),
-        DataLoader.loadCSV("car_8.csv", 2, 2),
-        DataLoader.loadCSV("car_9.csv", 2, 2),
-        DataLoader.loadCSV("car_10.csv", 2, 2),
-        DataLoader.loadCSV("car_11.csv", 2, 2),
-        DataLoader.loadCSV("car_12.csv", 2, 2)
+        DataLoader.loadCSV("car_1.csv", 2, 2)
+//        DataLoader.loadCSV("car_2.csv", 2, 2),
+//        DataLoader.loadCSV("car_3.csv", 2, 2),
+//        DataLoader.loadCSV("car_5.csv", 2, 2),
+//        DataLoader.loadCSV("car_6.csv", 2, 2),
+//        DataLoader.loadCSV("car_7.csv", 2, 2),
+//        DataLoader.loadCSV("car_8.csv", 2, 2),
+//        DataLoader.loadCSV("car_9.csv", 2, 2),
+//        DataLoader.loadCSV("car_10.csv", 2, 2),
+//        DataLoader.loadCSV("car_11.csv", 2, 2),
+//        DataLoader.loadCSV("car_12.csv", 2, 2)
     )
 
     val filtered = datas.map { data ->
@@ -99,13 +100,13 @@ fun main() {
         trainingData.addAll(data)
     }
 
-    val network = Track(trainingData, 4000, 100).singleNetwork(15)
+    val network = Track(trainingData, 10000, 100).singleNetwork(10)
 
     val testData = DataLoader.loadCSV("test.csv", 2, 2)
     val outputData = testData.input().map {
         Point(network.output(it))
     }
 
-    val chart = createChart("test123", testData.output(), testData.input(), outputData)
+    val chart = createChart("Korekta toru robota - RBF", testData.output(), testData.input(), outputData)
     Charts.saveChart("test", chart)
 }
